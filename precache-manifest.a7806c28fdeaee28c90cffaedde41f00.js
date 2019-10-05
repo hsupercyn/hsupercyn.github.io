026d89024e2cc3eed8ef08d67e7f46afbc31631e
Cynthia Hsu <cynthiaxhsu@berkeley.edu>
2019-10-05 13:04:15 -0500

    Updates

---
+++ precache-manifest.a7806c28fdeaee28c90cffaedde41f00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592560e9cdbf4776cfa51edb8b0c5abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31cc243d706f0dab59c0",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "8b84eb0a101bb17a556b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "31cc243d706f0dab59c0",
+    "url": "/static/js/2.93a0fc20.chunk.js"
+  },
+  {
+    "revision": "8b84eb0a101bb17a556b",
+    "url": "/static/js/main.9d6bcf0f.chunk.js"
+  },
+  {
+    "revision": "42c67b91adc7ac55185c",
+    "url": "/static/js/runtime-main.58660c70.js"
+  }
+]);
